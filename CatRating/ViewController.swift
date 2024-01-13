946bbf4eb21f120adb64ad54d84a2fdefcf1c490
Leonard <leonardrolandvictor@gmail.com>
2024-01-13 09:18:42 +0100

    Initial Commit

---
+++ CatRating/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatRating
+//
+//  Created by Leonard Roland on 13.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
